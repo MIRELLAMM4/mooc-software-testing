6d47f1e0f9ffe0bf5057d4323dd6956f12f26ae4
MIRELLAMM4 <mirella.mamani@ucsm.edu.pe>
2025-03-28 22:44:00 -0500

    test: Implementando  pruebas GHappy.java

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,55 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GHappyTest {
+
+    @Test
+    public void testAllHappyGs() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("gggg"));
+    }
+
+    @Test
+    public void testSingleG() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("g"));
+    }
+
+    @Test
+    public void testGAtStart() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("gabc"));
+    }
+
+    @Test
+    public void testGAtEnd() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("abcg"));
+    }
+
+    @Test
+    public void testGSeparated() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("agbgc"));
+    }
+
+    @Test
+    public void testGCluster() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("aggga"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy(""));
+    }
+
+    @Test
+    public void testNoG() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("abc"));
+    }
 }
