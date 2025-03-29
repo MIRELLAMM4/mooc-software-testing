8848aa1008421943ab9e4a4604effb2a2da2dcd7
MIRELLAMM4 <mirella.mamani@ucsm.edu.pe>
2025-03-28 22:59:21 -0500

    test: Implementando pruebas de CaesarShiftCipher.java

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,49 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    public void testShiftPositive() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        assertEquals("def", cipher.caesarShiftCipher("abc", 3));
+    }
+
+    @Test
+    public void testShiftNegative() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        assertEquals("xyz", cipher.caesarShiftCipher("abc", -3));
+    }
+
+    @Test
+    public void testShiftWrapAround() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        assertEquals("zab", cipher.caesarShiftCipher("xyz", 2));
+    }
+
+    @Test
+    public void testShiftZero() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        assertEquals("hello", cipher.caesarShiftCipher("hello", 0));
+    }
+
+    @Test
+    public void testInvalidCharacter() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        assertEquals("invalid", cipher.caesarShiftCipher("hello world", 3));
+    }
+
+    @Test
+    public void testEmptyString() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        assertEquals("", cipher.caesarShiftCipher("", 5));
+    }
+
+    @Test
+    public void testNullString() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        assertEquals("invalid", cipher.caesarShiftCipher(null, 5));
+    }
 }
